a180de96eb75c06c1439aa893aa3591883680de2
IndraneeK95 <100950322+IndraneeK95@users.noreply.github.com>
2022-09-20 17:03:57 +0530

    Updated problem08.c

---
+++ set01/problem08.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_n_array(int n, int a[n]);
+void output(int n, int a[n],int sum);
+
+int main()
+{
+     int n, sum;
+     n=input_array_size();
+     int a[n];
+     input_array(n,a);
+     sum=sum_n_array(n, &a);
+     output(n,&a,sum);
+     return 0;
+}
+
+int input_array_size()
+{
+    int n;
+    printf("enter how many numbers wants to be added\n");
+    scanf("%d", &n);
+    return n;
+}
+
+void input_array(int n, int a[n])
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("enter the number\n");
+        scanf("%d" , &a[i]);
+    }
+}
+
+int sum_n_array(int n, int a[n])
+{
+     int sum,i;
+     for(i=0;i<n;i++)
+     {
+     sum=sum + a[i];
+     printf("%d",a[i]);
+     }
+     
+     return sum;
+}
+
+void output(int n, int a[n], int sum)
+{
+    printf("The sum of the array is %d",sum);
+    
+}
