fe69591293beabfb4a4809df286ecc0fecb7c6a7
IndraneeK95 <100950322+IndraneeK95@users.noreply.github.com>
2022-09-20 14:17:58 +0530

    Update problem03.c

---
+++ set01/problem03.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
+
+
+int main()
+{
+    int a, b, sum;
+    printf("please enter the inputs\n");
+    a=input();
+    b=input();
+    sum= add(a,b);
+    output(a, b, sum);
+}
+int input()
+{
+    int a;
+    printf("enter the  number to add\n ");
+    scanf("%d", &a);
+    return a;
+}
+
+
+int add(int a, int b)
+{
+    int sum=a+b;
+    return sum;
+}
+
+void output(int a, int b, int sum)
+{
+    printf("The sum of %d %d", a,b);
+    printf(" is ");
+    printf("%d", sum);
+}
